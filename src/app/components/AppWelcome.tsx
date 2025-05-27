1ab3dc838e7b7b3434c742c3c7b9f08b9565434b
Panachaya <07530.panachaya@gmail.com>
2025-05-27 11:18:56 +0700

    learn conditional redering

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,19 @@
 export default function AppWelcome(){
+    const title = 'Welcome to cosci';
+    const currentYear =  <p>2025</p>;
+    const isShow = true;
+    
     return(
         <>
-            <p>Welcome to cosci</p>
+            <p>{title.toUpperCase()}</p>
+            {currentYear}
+            {
+                isShow && <p>Data: 10/10/1998</p>
+            }
+            {
+                isShow ? <p> hello Next.js </p> : <p>Hello JavaScript</p> 
+            }
+                
         </>
     );
 }
