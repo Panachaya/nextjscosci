242a19e5a84c83a7bcc303a57434c6f6e30c1109
Panachaya <07530.panachaya@gmail.com>
2025-05-28 16:02:40 +0700

    update env

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -2,11 +2,11 @@ import Contact01Page from "@/components/contact-01/contact-01";
 
 export default async function About(){
     const data = await fetch('https://api.codingthailand.com/api/version',{ next:{revalidate:3600} });
-    const apiInto = await data.json();
+    const apiInfo = await data.json();
     return(
         <>
         {
-            <Contact01Page version={apiInto.data.version}/>
+            <Contact01Page version={apiInfo.data.version}/>
         }
         </>
     );
--- src/components/navbar-01/nav-menu.tsx
+++ src/components/navbar-01/nav-menu.tsx
@@ -25,6 +25,12 @@ export const NavMenu = (props: NavigationMenuProps) => (
           <Link href="/about">เกี่ยวกับเรา</Link>
         </NavigationMenuLink>
       </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild>
+          <Link href="/course">คอร์ส</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
     </NavigationMenuList>
   </NavigationMenu>
+  
 );
