fa25e1b4d22c01516f345df701819c6993bb5afe
Panachaya <07530.panachaya@gmail.com>
2025-05-28 14:11:05 +0700

    add version

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,9 +1,14 @@
 import Contact01Page from "@/components/contact-01/contact-01";
 
-export default function About(){
+export default async function About(){
+    const data = await fetch('https://api.codingthailand.com/api/version');
+    const apiInto = await data.json();
     return(
         <>
-        <Contact01Page/>
+        <p>{JSON.stringify(apiInto)}</p>
+        {
+            <Contact01Page version={apiInto.data.version}/>
+        }
         </>
     );
 }
