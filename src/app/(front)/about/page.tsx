721638bf921681324071cd86ed3787f1b5e2ee50
Panachaya <07530.panachaya@gmail.com>
2025-05-28 14:20:21 +0700

    add version on page เกี่ยวกับเรา

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,11 +1,10 @@
 import Contact01Page from "@/components/contact-01/contact-01";
 
 export default async function About(){
-    const data = await fetch('https://api.codingthailand.com/api/version');
+    const data = await fetch('https://api.codingthailand.com/api/version',{ next:{revalidate:3600} });
     const apiInto = await data.json();
     return(
         <>
-        <p>{JSON.stringify(apiInto)}</p>
         {
             <Contact01Page version={apiInto.data.version}/>
         }
