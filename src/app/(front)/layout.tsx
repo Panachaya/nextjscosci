9d6f0cb6c4814d843791c1c8233f6ad9afbd6dde
Panachaya <07530.panachaya@gmail.com>
2025-05-28 11:13:11 +0700

    move some folder to folder (front) and (auth)

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { K2D } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Navbar01Page from "@/components/navbar-01/navbar-01";
 
 const k2D = K2D({
