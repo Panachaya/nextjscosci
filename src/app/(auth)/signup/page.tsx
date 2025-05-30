0ddc5a3842bbc21520037ad60caf3f06a478a66f
Panachaya <07530.panachaya@gmail.com>
2025-05-30 11:34:16 +0700

    update

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -15,6 +15,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { authClient } from "@/lib/auth-client"; //import the auth client
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   email: z.string().email("รูปแบบอีเมลล์ไม่ถูกต้อง"),
@@ -22,6 +24,7 @@ const formSchema = z.object({
 });
 
 const Signup01Page = () => {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     defaultValues: {
       email: "",
@@ -30,8 +33,26 @@ const Signup01Page = () => {
     resolver: zodResolver(formSchema),
   });
 
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log(data);
+  const onSubmit = async (form: z.infer<typeof formSchema>) => {
+    await authClient.signUp.email({
+      name:"Your Name",
+        email: form.email, // user email address
+        password: form.password, // user password -> min 6 characters by default
+    }, {
+        onRequest: (ctx) => {
+            //show loading
+            console.log(ctx.body);
+        },
+        onSuccess: (ctx) => {
+            //redirect to the dashboard or sign in page
+            console.log(ctx.data)
+            router.replace('/login');
+        },
+        onError: (ctx) => {
+            // display the error message
+            alert(ctx.error.message);
+        },
+});
   };
 
   return (
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,17 @@
 import { betterAuth } from "better-auth";
- import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import db from "@/db"; 
+import { account, session, user, verification } from "@/db/schema";
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql",
-    })
+        schema:{user,account,session,verification},
+        usePlural:false
+    }),
+    emailAndPassword:{
+        enabled:true,
+        autoSignIn:false,
+        requireEmailVerification:false,
+        minPasswordLength: 6
+    }
 })
